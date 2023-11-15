165938c4070d76a25842b0738e0246a137f1fe60
taufiqgiri <taufiq.ichwanusofa@ist.id>
2023-11-15 20:59:16 +0700

    Initial Commit

---
+++ login-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  login-uikit
+//
+//  Created by Taufiq Ichwanusofa on 15/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
